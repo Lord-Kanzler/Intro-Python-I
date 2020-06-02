6d11ac4105f6dbc8741e477b054dc88ac12aa0c3
Lord-Kanzler <climberalex@gmail.com>
2020-06-01 20:07:19 -0400

    finished assignment

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,8 @@
 # Print "Hello, world!" to your terminal
+
+import os
+
+print("Hello, world!")
+
+
+# print(f"Hello, {os.getlogin()}. How are you?")
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,19 @@
-# Write a function is_even that will return true if the passed-in number is even.
+# Write a function is_even that will return true if the passed-in number
+# is even.
 
 # YOUR CODE HERE
+def is_even(number):
+    if number % 2 == 0:
+        return "TRUE"
+
+    else:
+        return "FALSE"
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
-
+print("Even!" if is_even(num) == "TRUE" else "Odd")
